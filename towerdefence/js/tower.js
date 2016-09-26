2f029384a673b10dc5071a760945d79127b48165
dragonslayern <lovelidberg@hotmail.com>
2016-09-26 11:28:08 +0900

    everything works with placing towers and selling them

--- towerdefence/js/tower.js
+++ towerdefence/js/tower.js
@@ -1,10 +1,12 @@
-function Tower(x, y, id) {
+function Tower(x, y, id, damage, type, cost) {
 
 	this.x = x;
 	this.y = y;
-	this.damage = 10;
+	this.damage = damage;
 	this.level = 1;
 	this.id = id;
+	this.type = type;
+	this.cost = cost;
 
 	this.getLevel = function(){
 		return level;
